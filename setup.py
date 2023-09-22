88ec3585158f02277f2054bfa0900ac5f5cb08b3
MySlientWind <634790985@qq.com>
2023-09-22 10:27:41 +0800

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="asa",
+    version="1.0",
+    author="Tao Jing, Qinglin Ma",
+    author_email="jingt20@mails.tsinghua.edu.cn",
+    description="astro statistic assistant",
+    packages=find_packages()
+)
