313612413f9e78e2d7c19a5773f3651b882e8192
MySlientWind <634790985@qq.com>
2023-10-31 13:29:13 +0800

    weighted statistic

---
+++ asa/weighted_statistic.py
@@ -0,0 +1,68 @@
+import numpy as np
+from sklearn.neighbors import KernelDensity
+
+from .Bcorner import quantile as _quantile
+
+
+def quantile(x, q, weights=None, N_min=2):
+    '''
+    The defination of weighted quantile is different from DescrStatsW
+    We use the interpolation of cdf, but they use the ecdf
+    '''
+    return np.array(_quantile(x, q, weights, N_min)[0])
+
+
+def median(x, w=None):
+    return np.median(x) if w is None else quantile(x, 0.5, weights=w)
+
+
+def mean(x, w=None):
+    return np.mean(x) if w is None else np.average(x, weights=w)
+
+
+def std(x, w=None, ddof=0):  # sourcery skip: remove-unnecessary-else
+    '''
+    Note that the w here is considered as reliability weights.
+    However, the weights in DescrStatsW is frequency weights.
+
+    https://en.wikipedia.org/wiki/Weighted_arithmetic_mean#Weighted_sample_variance
+    
+    Because the ddof with weight is a so complex concept
+    We use the effect sample size for ddof correction in weighted std
+    https://en.wikipedia.org/wiki/Design_effect#Effective_sample_size
+    '''
+    if w is None:
+        return np.std(x, ddof=ddof)
+    else:
+        N_eff = get_effect_sample_size(w)
+        return np.sqrt(
+            np.average(
+                (x - mean(x, w))**2, weights=w) * N_eff / (N_eff - ddof))
+
+
+def std_mean(x, w=None, ddof=0):
+    '''
+    The ddof only affects the estimation of std
+    We use the effect sample size as the denominator
+    https://en.wikipedia.org/wiki/Design_effect#Effective_sample_size
+    '''
+    N = get_effect_sample_size(w) if w is not None else x.size
+    return std(x, w=w, ddof=ddof) / np.sqrt(N)
+
+
+def std_median(x, w=None, **kde_kwargs):
+    kde = KernelDensity(**kde_kwargs).fit(x.reshape(-1, 1), sample_weight=w)
+    m = median(x, w)
+    fm = np.exp(kde.score_samples(np.array([[m]])))
+    N = get_effect_sample_size(w) if w is not None else x.size
+    return 1 / (2 * fm * np.sqrt(N))[0]
+
+
+def q(x, w=None, q=0.5):
+    return quantile(x, q, weights=w)
+
+
+def get_effect_sample_size(w):
+    V1 = np.sum(w)
+    V2 = np.sum(w**2)
+    return V1**2 / V2
---
+++ test/test_weighted_statistic.py
@@ -0,0 +1,59 @@
+import numpy as np
+from statsmodels.stats.weightstats import DescrStatsW
+
+from asa.weighted_statistic import *
+
+class TestWeightedStatistic:
+
+    def test_effect_sample_size(self):
+        w = np.ones(100)
+        assert get_effect_sample_size(w) == 100
+
+    def test_shape(self):
+        x = np.random.normal(size=100)
+        w = np.random.uniform(size=100)
+
+        assert median(x, w).shape == ()
+        assert median(x).shape == ()
+
+        assert mean(x, w).shape == ()
+        assert mean(x).shape == ()
+
+        assert std(x, w).shape == ()
+        assert std(x).shape == ()
+
+        assert std_mean(x, w).shape == ()
+        assert std_mean(x).shape == ()
+
+        assert std_median(x, w).shape == ()
+        assert std_median(x).shape == ()
+
+        assert q(x, w).shape == ()
+        assert q(x).shape == ()
+
+    def test_res(self):
+        x = np.random.normal(size=100)
+        w = np.random.uniform(size=100)
+
+        assert mean(x) == np.mean(x)
+        assert median(x) == np.median(x)
+        assert std(x) == np.std(x)
+        assert std_mean(x) == np.std(x) / np.sqrt(len(x))
+
+        ds = DescrStatsW(x, weights=w)
+        assert np.isclose(mean(x, w), ds.mean)
+
+
+        assert np.isclose(std(x, ddof=1), np.std(x, ddof=1))
+        assert np.isclose(std(x), std(x, w=np.ones_like(x)))
+        assert np.isclose(std(x, ddof=1), std(x, w=np.ones_like(x), ddof=1))
+        assert np.isclose(std(x, ddof=1), std(x, w=np.ones_like(x)*2, ddof=1))
+
+        assert np.isclose(std_mean(x), std_mean(x, w=np.ones_like(x)))
+        assert np.isclose(std_mean(x, ddof=1), std_mean(x, w=np.ones_like(x), ddof=1))
+        assert np.isclose(std_mean(x, ddof=1), std_mean(x, w=np.ones_like(x)*2, ddof=1))
+
+        assert np.isclose(std_median(x), std_median(x, w=np.ones_like(x)))
+        assert np.isclose(std_median(x), std_median(x, w=np.ones_like(x) * 2))
+
+
